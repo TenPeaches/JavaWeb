ad4398b21711bfa85a01c1f2ca5169da1f5bae2c
TenPeaches <2060853750@qq.com>
2024-10-10 20:14:49 +0800

    Add files via upload

---
+++ itheima_web_project/springboot-web-config2/src/test/java/com/itheima/AutoConfigurationTests.java
@@ -0,0 +1,32 @@
+package com.itheima;
+
+import com.example.HeaderGenerator;
+import com.example.HeaderParser;
+import com.example.TokenParser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class AutoConfigurationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void testTokenParser(){
+        System.out.println(applicationContext.getBean("TokenParser"));
+    }
+
+    @Test
+    public void testHeaderParser(){
+        System.out.println(applicationContext.getBean("HeaderParser.class"));
+    }
+
+    @Test
+    public void testHeaderGenerator(){
+        System.out.println(applicationContext.getBean("HeaderGenerator.class"));
+    }
+
+}
